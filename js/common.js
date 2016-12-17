bd92d1111c673e9008c5c63b66d47d9ef67aef7a
Vlad Ovsyannikov <microgub@outlook.com>
2016-12-17 23:11:19 +0500

    commit11

--- js/common.js
+++ js/common.js
@@ -1,10 +1,17 @@
 $(function(){
+  $.mask.definitions['a']='[А-Яа-я]';
+  /* Фамилия */
+  $("#input-1").mask("aaa?aaaaaaaaaaaaa", {placeholder:" "});
+  /* Имя */
+  $("#input-2").mask("aaa?aaaaaaaaaaaaa", {placeholder:" "});
+  /* Отчество */
+  $("#input-3").mask("aaa?aaaaaaaaaaaaa", {placeholder:" "}); 
   /* Серия и номер паспорта */
   $("#input-4").mask("99-99 999999", {placeholder:" "});
-
   /* Телефон */
-  $("#input-6").mask("(999) 999-9999", {placeholder:" "});
-    
+  $("#input-6").mask("+7(999)999-9999", {placeholder:" "});    
   /* Индекс */
-  $("#input-9").mask("999999", {placeholder:" "});
-});
+  $("#input-8").mask("999999", {placeholder:" "});
+  /* Населённый пункт */
+  $("#input-9").mask("aaaaa?aaaaaaaaaaaaa", {placeholder:" "});
+}); 
