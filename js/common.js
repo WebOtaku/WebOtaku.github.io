9d51473567c44aec69309c66694a39f491ee6b90
WebOtaku <microgub@outlook.com>
2016-12-19 08:53:41 +0600

    correct mask

--- js/common.js
+++ js/common.js
@@ -1,11 +1,11 @@
 $(function(){
-  $.mask.definitions['a']='[А-Яа-я]';
+  $.mask.definitions['а']='[А-Яа-я]';
   /* Фамилия */
-  $("#input-1").mask("aaa?aaaaaaaaaaaaa", {placeholder:" "});
+  $("#input-1").mask("ааа?ааааааааааааа", {placeholder:" "});
   /* Имя */
-  $("#input-2").mask("aaa?aaaaaaaaaaaaa", {placeholder:" "});
+  $("#input-2").mask("ааа?ааааааааааааа", {placeholder:" "});
   /* Отчество */
-  $("#input-3").mask("aaa?aaaaaaaaaaaaa", {placeholder:" "}); 
+  $("#input-3").mask("ааа?ааааааааааааа", {placeholder:" "}); 
   /* Серия и номер паспорта */
   $("#input-4").mask("99-99 999999", {placeholder:" "});
   /* Телефон */
@@ -13,5 +13,5 @@ $(function(){
   /* Индекс */
   $("#input-8").mask("999999", {placeholder:" "});
   /* Населённый пункт */
-  $("#input-9").mask("aaaaa?aaaaaaaaaaaaa", {placeholder:" "});
+  $("#input-9").mask("ааа?ааааааааааааааа", {placeholder:" "});
 }); 
