0a5fd0526fa7f1c4cc4be2a9c04b126df15b8986
WebOtaku <microgub@outlook.com>
2016-12-22 10:05:33 +0600

    correct mask3

--- js/common.js
+++ js/common.js
@@ -1,17 +1,16 @@
-$(function(){
-  $.mask.definitions['b']='[А-Яа-я]';
-  /* Фамилия */
+/*$(function(){
+ 
   $("#input-1").mask("bbb?bbbbbbbbbbbbb", {placeholder:" "});
-  /* Имя */
+
   $("#input-2").mask("bbb?bbbbbbbbbbbbb", {placeholder:" "});
-  /* Отчество */
+
   $("#input-3").mask("bbb?bbbbbbbbbbbbb", {placeholder:" "}); 
-  /* Серия и номер паспорта */
+ 
   $("#input-4").mask("99-99 999999", {placeholder:" "});
-  /* Телефон */
+
   $("#input-6").mask("+7(999)999-9999", {placeholder:" "});    
-  /* Индекс */
+
   $("#input-8").mask("999999", {placeholder:" "});
-  /* Населённый пункт */
+
   $("#input-9").mask("bbb?bbbbbbbbbbbbbbb", {placeholder:" "});
-}); 
+});*/
