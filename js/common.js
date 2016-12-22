0a47007bfb0572bf004c6893fd9e65630756ad79
WebOtaku <microgub@outlook.com>
2016-12-22 10:22:51 +0600

    mask is return

--- js/common.js
+++ js/common.js
@@ -1,5 +1,9 @@
-$(function(){
-  $("#input-4").mask("99-99 999999");
-  $("#input-6").mask("+7(999)999-9999");    
-  $("#input-8").mask("999999");
-});
+/*$(function(){
+  $.mask.definition['b']='[А-Яа-я]';
+  $("#input-1").mask("bbb?bbbbbbbbbbbbb", {placeholder:" "});
+  $("#input-2").mask("bbb?bbbbbbbbbbbbb", {placeholder:" "});
+  $("#input-3").mask("bbb?bbbbbbbbbbbbb", {placeholder:" "});
+  $("#input-4").mask("99-99 999999", {placeholder:" "});
+  $("#input-6").mask("+7(999)999-9999", {placeholder:" "});    
+  $("#input-8").mask("999999", {placeholder:" "});
+});*/
