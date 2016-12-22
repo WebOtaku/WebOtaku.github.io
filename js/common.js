7c31873403bb076718b974ca2ac1f19eda1148ae
WebOtaku <microgub@outlook.com>
2016-12-22 10:38:59 +0600

    test new mask

--- js/common.js
+++ js/common.js
@@ -1,9 +1,8 @@
-/*$(function(){
-  $.mask.definition['b']='[А-Яа-я]';
-  $("#input-1").mask("bbb?bbbbbbbbbbbbb", {placeholder:" "});
-  $("#input-2").mask("bbb?bbbbbbbbbbbbb", {placeholder:" "});
-  $("#input-3").mask("bbb?bbbbbbbbbbbbb", {placeholder:" "});
-  $("#input-4").mask("99-99 999999", {placeholder:" "});
-  $("#input-6").mask("+7(999)999-9999", {placeholder:" "});    
-  $("#input-8").mask("999999", {placeholder:" "});
-});*/
+$(function(){
+$.jMaskGlobals = {translation: {
+               'n': {pattern: /\d/},
+           }
+       };
+       $('#input-6').mask('+7(nnn)nnn-nnnn').val('+7');
+       $('#input-8').mask('nnnnnn');
+});
