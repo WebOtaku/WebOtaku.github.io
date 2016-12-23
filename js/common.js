fe30a6e3efca32a122f028141fcab75ba6875a75
WebOtaku <microgub@outlook.com>
2016-12-23 14:24:22 +0600

    new mask

--- js/common.js
+++ js/common.js
@@ -3,6 +3,7 @@ $.jMaskGlobals = {translation: {
                'n': {pattern: /\d/},
            }
        };
-       $('#input-6').mask('+7(nnn)nnn-nnnn').val('+7');
-       $('#input-8').mask('nnnnnn');
+       $('#input-4').mask('nn-nn nnnnnn');
+       $('#input-6').mask('+7(nnn)nnn-nn-nn').val('+7');
+       $('#input-8').mask('nnnnnn'); 
 });
